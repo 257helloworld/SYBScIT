ee23d2fddb9d2dbe3c1dcb7bf6e582ada8fec79c
257helloWorld <110030634+257helloWorld@users.noreply.github.com>
2022-12-18 20:30:32 +0530

    Commit

--- CGA/Practical_13.cpp
+++ CGA/Practical_13.cpp
@@ -18,10 +18,10 @@ void drawOnAxis(double x1, double y1, double x2, double y2) {
 void clockWise(double x1, double y1, double x2, double y2, double angle) {
 	double xn1, yn1, xn2, yn2;
 	double nangle = (angle * 3.14)/180;
-	xn1 = (x1 * cos(nangle)) + (y1 * sin(nangle));
-	yn1 = (y1 * cos(nangle)) - (x1 * sin(nangle));
-	xn2 = (x2 * cos(nangle)) + (y2 * sin(nangle));
-	yn2 = (y2 * cos(nangle)) - (x2 * sin(nangle));
+	xn1 = (x1 * cos(nangle)) - (y1 * sin(nangle));
+	yn1 = (y1 * cos(nangle)) + (x1 * sin(nangle));
+	xn2 = (x2 * cos(nangle)) - (y2 * sin(nangle));
+	yn2 = (y2 * cos(nangle)) + (x2 * sin(nangle));
 	
 	drawOnAxis(xn1, yn1, xn2, yn2);
 }
@@ -29,10 +29,10 @@ void clockWise(double x1, double y1, double x2, double y2, double angle) {
 void antiClockWise(double x1, double y1, double x2, double y2, double angle) {
 	double xn1, yn1, xn2, yn2;
 	double nangle = (angle * 3.14)/180;
-	xn1 = (x1 * cos(nangle)) - (y1 * sin(nangle));
-	yn1 = (y1 * cos(nangle)) + (x1 * sin(nangle));
-	xn2 = (x2 * cos(nangle)) - (y2 * sin(nangle));
-	yn2 = (y2 * cos(nangle)) + (x2 * sin(nangle));
+	xn1 = (x1 * cos(nangle)) + (y1 * sin(nangle));
+	yn1 = (y1 * cos(nangle)) - (x1 * sin(nangle));
+	xn2 = (x2 * cos(nangle)) + (y2 * sin(nangle));
+	yn2 = (y2 * cos(nangle)) - (x2 * sin(nangle));
 	
 	drawOnAxis(xn1, yn1, xn2, yn2);
 }
@@ -66,10 +66,10 @@ void main() {
 	setlinestyle(DOTTED_LINE,1,1);
 	switch(mode) {
 		case 1: 
-			antiClockWise(x1,y1,x2,y2,angle);
+			clockWise(x1,y1,x2,y2,angle);
 			break;
 		case 2:
-			clockWise(x1,y1,x2,y2,angle);
+			AntiClockWise(x1,y1,x2,y2,angle);
 			break;
 		default: 
 			cout << "Invalid Mode";
